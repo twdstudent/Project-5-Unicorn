96bb0619b52ca733fbb60417a9b8fa749cd3fcf3
Thomas Dodson <tw.dodson@yahoo.co.uk>
2019-08-25 20:59:35 +0000

    further testing carried out on bugs and features views and are now all passing

--- bugs/tests_views.py
+++ bugs/tests_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.shortcuts import get_object_or_404
 from .models import Bugs
-
+from django.contrib.auth.models import User
+from .forms import BugsPostForm
 
 class TestViews(TestCase):
 
@@ -10,18 +11,50 @@ class TestViews(TestCase):
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "base.html")
         
-#    def test_get_add_bug_page(self):
-#        page = self.client.get("/new_bug")
-#        self.assertEqual(page.status_code, 200)
-#        self.assertTemplateUsed(page, "bug-post-form.html")    
-#    
-#    def test_get_edit_item_page(self):
-#        bug = Bugs(title="Create a Test")
-#        bug.save()
-#
-#        page = self.client.get("/edit_bug/{0}".format(bug.id))
-#        self.assertEqual(page.status_code, 200)
-#        self.assertTemplateUsed(page, "bug-detail.html")
+    def test_get_add_bug_page(self):
+        User.objects.create_user(
+            username='TestBugPost1',
+            email='TestBug1@example.com',
+            password='password1')
+
+        bug = Bugs.objects.create(title='Test Bug Post',
+                                   content='Test Bug Post Content')
+
+        self.assertEqual(bug.content, 'Test Bug Post Content')
+
+        page = self.client.get("/bugs/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "bugs.html")
+
+        bug.content = 'Test bug Post Edit Content'
+        bug.save()
+
+        self.assertEqual(bug.title, 'Test Bug Post')
+        self.assertEqual(bug.content, 'Test bug Post Edit Content')
+
+        response = self.client.get('/bugs/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "bugs.html")
+    
+    def test_get_edit_bug_page(self):
+        User.objects.create_user(
+            username='TestBug1',
+            email='TestBugPost1@example.com',
+            password='password1')
+        self.client.login(username='TestBugPost1', password='password1')
+
+        bug = Bugs.objects.create(title='Test Bug Post',
+                                   content='Test Bug Content')
+
+        page = self.client.get('/bugs/')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "bugs.html")
+
+        bug.content = 'Test Bug Content'
+        bug.save()
+
+        self.assertEqual(bug.content,
+                         'Test Bug Content')
         
     def test_get_edit_page_for_item_that_does_not_exist(self):
         page = self.client.get("/1")
--- feature/test_views.py
+++ feature/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.shortcuts import get_object_or_404
 from .models import Feature
+from django.contrib.auth.models import User
+from .forms import FeaturesPostForm
 
 class TestViews(TestCase):
 
@@ -9,18 +11,30 @@ class TestViews(TestCase):
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "base.html")
         
-#    def test_get_add_bug_page(self):
-#        page = self.client.get("/new_bug")
-#        self.assertEqual(page.status_code, 200)
-#        self.assertTemplateUsed(page, "bug-post-form.html")    
-#    
-#    def test_get_edit_item_page(self):
-#        bug = Bugs(title="Create a Test")
-#        bug.save()
-#
-#        page = self.client.get("/edit_bug/{0}".format(bug.id))
-#        self.assertEqual(page.status_code, 200)
-#        self.assertTemplateUsed(page, "bug-detail.html")
+    def test_get_add_feature_page(self):
+        User.objects.create_user(
+            username='TestBugPost1',
+            email='TestBug1@example.com',
+            password='password1')
+
+        feature = Feature.objects.create(title='Test Feature Post',
+                                   content='Test Feature Post Content')
+
+        self.assertEqual(feature.content, 'Test Feature Post Content')
+
+        page = self.client.get("/feature/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "feature.html")
+
+        feature.content = 'Test Feature Post Edit Content'
+        feature.save()
+
+        self.assertEqual(feature.title, 'Test Feature Post')
+        self.assertEqual(feature.content, 'Test Feature Post Edit Content')
+
+        response = self.client.get('/feature/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "feature.html")
         
     def test_get_edit_page_for_item_that_does_not_exist(self):
         page = self.client.get("/1")
