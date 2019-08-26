afd7b8ff32c58e2efdf85cab990209669d6d2b03
Thomas Dodson <tw.dodson@yahoo.co.uk>
2019-08-26 17:19:46 +0000

    made mirgartions for heroku deployment

--- bugs/urls.py
+++ bugs/urls.py
@@ -3,8 +3,8 @@ from .views import get_bugs, bugs_detail, create_or_edit_bug, upvote_bug
 
 urlpatterns = [
     url(r'^$', get_bugs, name='get_bugs'),
-    url(r'^upvote(?P<pk>\d+)/$', upvote_bug, name='upvote_bug'),
     url(r'^(?P<pk>\d+)/$', bugs_detail, name='bugs_detail'),
+    url(r'^upvote(?P<pk>\d+)/$', upvote_bug, name='upvote_bug'),
     url(r'^new/$', create_or_edit_bug, name='new_bug'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug')
 ]
